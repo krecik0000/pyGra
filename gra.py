c6bd88fc3e37a68c13e51ac17e2d060423b272ec
krecik0000 <milo04v2@gmail.com>
2020-07-22 11:31:55 +0200

    add newGame def

--- gra.py
+++ gra.py
@@ -9,6 +9,28 @@ def valueException():
     time.sleep(1)
     os.system("cls")
 
+def newGame(choiceMenu):
+    while choiceMenu != 4:
+        print("Select difficulty level:")
+        print("|1|Easy")
+        print("|2|Normal")
+        print("|3|Hard")
+        print("|4|Back")
+        try:
+            choiceMenu = int(input(":"))
+            if choiceMenu == 1:
+                print("1")
+            elif choiceMenu == 2:
+                print("2")
+            elif choiceMenu == 3:
+                print("3")
+            elif choiceMenu != isinstance(choiceMenu,str) and choiceMenu != 3 and choiceMenu != 1 and choiceMenu != 2 and choiceMenu != 4:
+                print("Ops! try use correct numbers")
+                time.sleep(1)
+                os.system("cls")
+        except ValueError:
+            valueException()
+
 def play(choiceMenu):
     while choiceMenu != 3:  #Menu loop which will be executed until you click "3" button
         print("|1|New Game")
@@ -18,7 +40,7 @@ def play(choiceMenu):
             choiceMenu = int(input(":"))
             os.system("cls")
             if choiceMenu == 1:
-                print("1")
+                newGame(choiceMenu)
             elif choiceMenu == 2:
                 print("2")
             elif choiceMenu != isinstance(choiceMenu,str) and choiceMenu != 3 and choiceMenu != 1 and choiceMenu != 2:
@@ -28,7 +50,6 @@ def play(choiceMenu):
         except ValueError: #When input value is not a number print this exception
             valueException()
             
-
 def menu(choiceMenu):
     while choiceMenu != 3:  #Menu loop which will be executed until you click "3" button
         print("GAME NAME")
