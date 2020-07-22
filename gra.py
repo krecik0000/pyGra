48fb10d4d7a6aa9c81a9c37d3a3a33032cd1e11a
krecik0000 <milo04v2@gmail.com>
2020-07-22 11:24:16 +0200

    add play menu

--- gra.py
+++ gra.py
@@ -2,33 +2,53 @@ import random
 import os 
 import time
 
-def play():
-    print("1")
+choiceMenu = 0
+def valueException():
+    os.system("cls")
+    print("Ops! try use number keys")
+    time.sleep(1)
+    os.system("cls")
 
-def menu():
-    choiceMenu = 0
+def play(choiceMenu):
+    while choiceMenu != 3:  #Menu loop which will be executed until you click "3" button
+        print("|1|New Game")
+        print("|2|Load Game")
+        print("|3|Back")
+        try: #Try input number
+            choiceMenu = int(input(":"))
+            os.system("cls")
+            if choiceMenu == 1:
+                print("1")
+            elif choiceMenu == 2:
+                print("2")
+            elif choiceMenu != isinstance(choiceMenu,str) and choiceMenu != 3 and choiceMenu != 1 and choiceMenu != 2:
+                print("Ops! try use correct numbers")
+                time.sleep(1)
+                os.system("cls")
+        except ValueError: #When input value is not a number print this exception
+            valueException()
+            
+
+def menu(choiceMenu):
     while choiceMenu != 3:  #Menu loop which will be executed until you click "3" button
         print("GAME NAME")
         print("|1|Play")
-        print("|2|Upgrades")
+        print("|2|Settings")
         print("|3|Exit")
         try: #Try input number
             choiceMenu = int(input(":")) 
             os.system("cls")
             if choiceMenu == 1:          
-                play()
+                play(choiceMenu)
             elif choiceMenu == 2:
-                #upgrades()
+                #settings()
                 print("2")
             elif choiceMenu != isinstance(choiceMenu,str) and choiceMenu != 3 and choiceMenu != 1 and choiceMenu != 2:
                 print("Ops! try use correct numbers")
                 time.sleep(1)
                 os.system("cls")
         except ValueError:     #When input value is not a number print this exception
-            os.system("cls")
-            print("Ops! try use number keys")
-            time.sleep(1)
-            os.system("cls")
+            valueException()
 
 os.system("cls")
-menu()
+menu(choiceMenu)
