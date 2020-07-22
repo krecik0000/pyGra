68a8ad37a6b73f388b3825d510365edf440e5f53
krecik0000 <milo04v2@gmail.com>
2020-07-22 10:45:30 +0200

    update menu exceptions comments

--- gra.py
+++ gra.py
@@ -4,13 +4,13 @@ import time
 
 choiceMenu = 0
 
-while choiceMenu != 3:
+while choiceMenu != 3:  #Menu loop which will be executed until you click "3" button
     print("|1|Play")
     print("|2|Upgrades")
     print("|3|Exit")
-    try:
-        choiceMenu = int(input(":"))
-        if choiceMenu == 1:
+    try: #Try input number
+        choiceMenu = int(input(":")) 
+        if choiceMenu == 1:          
             #play()
             print("1")
         elif choiceMenu == 2:
@@ -18,5 +18,5 @@ while choiceMenu != 3:
             print("2")
         elif choiceMenu != isinstance(choiceMenu,str) and choiceMenu != 3 and choiceMenu != 1 and choiceMenu != 2:
                 print("Ops! try use correct numbers")
-    except ValueError:
+    except ValueError:     #When input value is not a number print this exception
         print("Ops! try use number keys")
