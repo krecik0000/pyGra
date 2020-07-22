add0b5c472c3a31ea182dd19fe73dbc8ce7a01c7
krecik0000 <milo04v2@gmail.com>
2020-07-22 10:26:46 +0200

    update menu exceptions

--- gra.py
+++ gra.py
@@ -8,12 +8,15 @@ while choiceMenu != 3:
     print("|1|Play")
     print("|2|Upgrades")
     print("|3|Exit")
-    choiceMenu = int(input(":"))
-    if choiceMenu == 1:
-        #play()
-        print("1")
-    elif choiceMenu == 2:
-        #upgrades()
-        print("2")
-    elif choiceMenu != 3 and choiceMenu != 1 and choiceMenu != 2:
-        print("invalid keys pressed")
+    try:
+        choiceMenu = int(input(":"))
+        if choiceMenu == 1:
+            #play()
+            print("1")
+        elif choiceMenu == 2:
+            #upgrades()
+            print("2")
+        elif choiceMenu != isinstance(choiceMenu,str) and choiceMenu != 3 and choiceMenu != 1 and choiceMenu != 2:
+                print("Ops! try use correct numbers")
+    except ValueError:
+        print("Ops! try use number keys")
