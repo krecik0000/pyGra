7ea9b31a357d5dcb9723c8e8c61bf9956ce51f4a
krecik0000 <milo04v2@gmail.com>
2020-07-22 10:13:01 +0200

    add menu options

--- gra.py
+++ gra.py
@@ -9,3 +9,11 @@ while choiceMenu != '3':
     print("|2|Upgrades")
     print("|3|Exit")
     choiceMenu = input(":")
+    if choiceMenu == '1':
+        #play()
+        print("1")
+    elif choiceMenu == '2':
+        #upgrades()
+        print("2")
+    elif choiceMenu != 3 and choiceMenu != '1' and choiceMenu != '2':
+        print("invalid keys pressed")
