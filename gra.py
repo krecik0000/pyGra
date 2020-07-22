cf9b4d8966a4104c1715ae08b2e4182f41e8783c
krecik0000 <milo04v2@gmail.com>
2020-07-22 10:07:48 +0200

    add menu

--- gra.py
+++ gra.py
@@ -2,3 +2,10 @@ import random
 import os 
 import time
 
+choiceMenu = 0
+
+while choiceMenu != '3':
+    print("|1|Play")
+    print("|2|Upgrades")
+    print("|3|Exit")
+    choiceMenu = input(":")
